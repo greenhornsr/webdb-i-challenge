3fb543f11234b2ff6c33059e1a5eae5a596be8fd
greenhornsr <srollins11279@gmail.com>
2019-06-03 14:16:52 -0700

    set up index / get and server.get(/accounts).

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const db = require('./data/accounts-model');
 
 const server = express();
 
-// your code here
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Greetings</h1>`)
+})
+
+server.get('/accounts', (req, res) => {
+    db.find()
+    .then(accounts => {
+        res.status(200).json({success: true, accounts})
+    })
+    .catch(err => {
+        res.status(400).json({success: false, message: 'no accounts found', err })
+    })
+})
+
 
 module.exports = server;
